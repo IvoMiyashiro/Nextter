49bd2a9a600738094d5be576a073fd2207701ccd
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2021-11-09 23:15:55 -0300

    finishig home page

---
+++ components/AppLayout.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from 'next';
+import {fonts, colors} from '../styles/theme';
+
+export const AppLayout: NextPage = ({ children }) => {
+  return(
+    <>
+      <main>
+        {children}
+      </main>
+      <style jsx global>{`
+         html,
+         body {
+            background: ${colors.background};
+            font-family: ${fonts.base};
+            margin: 0;
+            padding: 0;
+         }
+
+         * {
+            box-sizing: border-box;
+         }
+
+         h1,
+         h2,
+         h3,
+         p {
+            margin: 0;
+         }
+
+         a {
+          text-decoration: none;
+          color: white;
+        }
+         `}
+      </style>
+    </>
+  );
+};
---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+export const fonts = {
+  base: `'JetBrains Mono', monospace`,
+};
+
+export const colors = {
+  background: '#0A192F',
+  backgroundAlt: '#262626',
+  primary: '#59E3C5',
+  title: '#CCD6F6',
+  text: '#838DAA',
+};
